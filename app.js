8f13220b7680b4d5c0355561146217558ddd80bf
syrohei <syrohei@gmail.com>
2016-06-10 05:43:57 +0900

    fix

--- app.js
+++ app.js
@@ -1,41 +1,36 @@
+/*global require*/
+/*global process*/
+/*global console*/
+/*eslint-env es6*/
+/*eslint prefer-const: "error"*/
+/*eslint no-console: ["error", { allow: ["warn", "error","log"] }] */
+
 'use strict';
 
-var express = require('express');
-var Botkit = require('botkit')
-var CronJob = require('cron').CronJob;
-var controller = Botkit.slackbot({
-  debug:true,
-})
-var app = express();
+const express = require('express');
+const config = require('config');
+const Botkit = require('botkit')
+const rp = require('request-promise');
+const CronJob = require('cron').CronJob;
+const Store = require("jfs");
 
-var bot = controller.spawn({
-  token: process.env.token
+
+const token = config.token
+const channel = config.channel
+const store_path = config.store_path
+const Storage = new Store(store_path)
+
+const app = express();
+
+const controller = Botkit.slackbot({
+  json_file_store: store_path,
+  debug:true
+})
+const bot = controller.spawn({
+  token: token
 }).startRTM()
 
-
-//conversation
-controller.hears(['hello','hi'], 'direct_message,direct_mention,mention', function(bot, message){
-  bot.api.reactions.add({
-    timestamp: message.ts,
-    channel: message.channel,
-    name: 'robot_face',
-  },function(err, res){
-    if (err){
-      bot.botkit.log('Failed to add emoji', err)
-    }
-  })
-
-  controller.storage.users.get(message.user, function(err, user) {
-    if (user && user.name){
-      bot.reply(message, 'Hello' + user.name + '!!')
-    }else{
-      bot.reply(message, reply_with_attachments)
-    }
-  })
-})
-
-
-var reply_with_attachments = {
+const reply_with_attachments = {
   'username': 'Qiitachan',
   'text': 'This is a pre-text',
   'attachments': [
@@ -49,20 +44,93 @@ var reply_with_attachments = {
   //'icon_url': 'http://lorempixel.com/48/48',
   'icon_emoji': ':sushi'
 }
+//conversation
+controller.hears(['hello','hi'], 'direct_message,direct_mention,mention', (bot, message) => {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face'
+  },(err, res) => {
+    if (err){
+      bot.botkit.log('Failed to add emoji', err)
+    }else {
+      bot.botkit.log(res)
+    }
+  })
 
-var channel = "D1FBT1UP2"
-//cron job 
-var cron = new CronJob({
-  cronTime: '00 * * * * *', 
-  onTick: function() {
-    bot.say({
-      channel: channel,
-      text: 'よろしくお願いします！'
-    }, function(err) {
-      if (err) {
-        bot.botkit.log(err);
-      }
-    });
+  controller.storage.users.get(message.user, (err, user) => {
+    if (user && user.name){
+      bot.reply(message, 'Hello' + user.name + '!!')
+    }else{
+      bot.reply(message, reply_with_attachments)
+    }
+  })
+})
+
+controller.hears(['all','ぽん'], 'direct_message,direct_mention,mention', (bot, message) => {
+  Storage.get("qiita_items", (err, old_data) => {
+    if (old_data && old_data.items){
+      const _old = old_data.items
+      _old.forEach((item) => {
+        bot.say({
+          channel: message.channel,
+          text: item.title + "\n" + item.url
+        })
+      })
+    }
+  })
+})
+
+//cron job
+const cron = new CronJob({
+  cronTime: '00 * * * * *',
+  onTick: () => {
+    const url = "http://qiita.com/api/v2/items?page=1&per_page=20&query=solidity"
+    rp(url)
+      .then((new_data) => {
+        Storage.get("qiita_items", (err, old_data) => {
+          const _new = JSON.parse(new_data)
+          if (old_data && old_data.items){
+            const _old = old_data.items
+            Storage.save("qiita_items", {
+              items: _new
+            }, (err) => {
+              if (!err){
+                console.log(_new[0].title)
+                console.log(_old[0].title)
+
+                if (_new[0].id != _old[0].id){
+                  const say = () => {
+                    bot.say({
+                      channel: channel,
+                      text: _new[0].title + "\n" + _new[0].url
+                    }, (err) => {
+                      if (err) {
+                        bot.botkit.log(err);
+                        say()
+                      }
+                    })
+                  }
+                  say()
+                }
+              }
+            })
+          }else{
+            Storage.save( "qiita_items", {
+              items: _new
+            }, (err) => {
+              if (err){
+                console.log(err);
+              }
+            })
+          }
+        })
+      })
+        // Process html...
+      .catch(function (err) {
+          console.log(err);
+        // Crawling failed...
+      })
   },
   start: false,
   timeZone: 'Asia/Tokyo'
@@ -71,7 +139,7 @@ cron.start();
 
 
 //request events
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   bot.say({ channel: channel, text: "requestが来たよ! \n ```" + JSON.stringify(req.route) + " \n```"})
   res.status(200).send('hello bots!');
 });
